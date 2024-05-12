a01d95030cea69f3c6c88f0eb258c12a7684f636
ZaharSirko <magezku@gmail.com>
2024-05-12 21:45:49 +0300

    commit restcontreollers

--- src/main/java/com/example/webshopspring/controllers/MainController.java
+++ src/main/java/com/example/webshopspring/controllers/MainController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class MainController {
-final private GoodService goodService;
+final private GoodService goodService;asdas
 
     @Autowired
     public MainController(GoodService goodService) {
